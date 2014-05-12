7a1dc34b1f7e593fc727fb64e36de9f2182fe0e2
Michael Sullivan <sully@msully.net>
2014-05-12 17:57:22 +0000

    More test

---
+++ leapfrog-test.c
@@ -0,0 +1,69 @@
+// Try a leapfrogging thing
+// We are looking for ry1==1, ry2==2, rx2==0
+
+#include "atomic.h"
+#include <stdio.h>
+
+
+volatile long x PADDED = 1;
+volatile long y PADDED = 1;
+
+int thread0()
+{
+    x = 1;
+    smp_mb();
+    y = 1;
+    return 0;
+}
+
+int thread1()
+{
+    int ry = y;
+    y = 2;
+    return ry;
+}
+
+int thread2()
+{
+    int ry;
+    // Can't do a ctrl dep because it might not happen
+    //while ((ry = y) != 2);
+    ry = y;
+    int rx = x;//*bullshit_dep(&x, ry);
+    return (rx<<2) | ry;
+}
+
+
+void reset()
+{
+    x = y = 0;
+}
+
+// Formatting results
+int result_counts[16];
+
+void process_results(int *r)
+{
+    int idx = r[2] | (r[1] << 3);
+    result_counts[idx]++;
+}
+
+void summarize_results()
+{
+    for (int ry1 = 0; ry1 <= 1; ry1++) {
+        for (int ry2 = 0; ry2 <= 2; ry2++) {
+            for (int rx2 = 0; rx2 <= 1; rx2++) {
+                int idx = (ry1<<3) | (rx2<<2) | ry2;
+                printf("ry1=%d ry2=%d rx2=%d: %d\n", ry1, ry2, rx2,
+                       result_counts[idx]);
+            }
+        }
+    }
+    printf("&x = %p, &y=%p\n", &x, &y);
+}
+
+
+typedef int (test_fn)();
+test_fn *test_fns[] = {thread0, thread1, thread2};
+
+int thread_count = 3;
--- wrc-test.c
+++ wrc-test.c
@@ -1,5 +1,5 @@
 // Try wrc
-// This never actually happens on my arm machine.
+// This works
 
 #include "atomic.h"
 #include <stdio.h>
@@ -17,14 +17,19 @@ int thread0()
 int thread1()
 {
     int rx;
-    while (!(rx = x));
+    // Can't observe this with a ctrl dep here! Make sense since writes
+    // won't get speculated before control dependent reads!
+    //while (!(rx = x));
+    rx = x;
     y = 1;
-    return 0;
+    return rx;
 }
 
 int thread2()
 {
     int ry;
+    // I manage to observe this with a ctrl dep, but not without one!
+    //ry = y;
     while (!(ry = y));
     int rx = x;
     return (rx<<1) | ry;
@@ -37,20 +42,23 @@ void reset()
 }
 
 // Formatting results
-int result_counts[4];
+int result_counts[8];
 
 void process_results(int *r)
 {
-    int idx = r[2];
+    int idx = r[2] | (r[1] << 2);
     result_counts[idx]++;
 }
 
 void summarize_results()
 {
-    for (int r0 = 0; r0 <= 1; r0++) {
-        for (int r1 = 0; r1 <= 1; r1++) {
-            int idx = (r1<<1) | r0;
-            printf("r0=%d r1=%d: %d\n", r0, r1, result_counts[idx]);
+    for (int rx = 0; rx <= 1; rx++) {
+        for (int r0 = 0; r0 <= 1; r0++) {
+            for (int r1 = 0; r1 <= 1; r1++) {
+                int idx = (rx<<2) | (r1<<1) | r0;
+                printf("rx=%d r0=%d r1=%d: %d\n", rx, r0, r1,
+                       result_counts[idx]);
+            }
         }
     }
     printf("&x = %p, &y=%p\n", &x, &y);
