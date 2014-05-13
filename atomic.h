5102a35fe68b7f858798609c0cbcb58e6f2706dc
Michael Sullivan <sully@msully.net>
2014-05-13 19:15:01 +0000

    Implement ctrlisync

--- atomic.h
+++ atomic.h
@@ -37,6 +37,7 @@
     })
 
 // On x86 we don't need this
+#define ctrl_isync(x) do { } while(0)
 #define dependent_zero(x) 0
 
 #elif defined(__arm__)
@@ -45,18 +46,40 @@
 #define smp_rmb smp_mb
 #define smp_wmb smp_mb
 
+#define ctrl_isync(v)                                   \
+    __asm__ __volatile__("cmp %[val], %[val];"          \
+                         "beq 1f;"                      \
+                         "1: isb":  :                   \
+                         [val] "r" (v) : "memory")
+
+// In the one simple test I tried, using ctrl_isync didn't seem any better
+#define USE_CTRL_ACQUIRE 1
+
 /* These are for ARM */
 #define smp_store_release(p, v)                  \
     do {                                         \
         smp_mb();                                \
         ACCESS_ONCE(*p) = v;                     \
     } while (0)
+#if USE_CTRL_ACQUIRE
+/* Use ctrlisync to do acquire */
+#define smp_load_acquire(p)                      \
+    ({                                           \
+    __typeof__(*p) __v = ACCESS_ONCE(*p);        \
+    ctrl_isync(__v);                             \
+    __v;                                         \
+    })
+
+#else
+/* Just emit a dmb for it */
 #define smp_load_acquire(p)                      \
     ({                                           \
     __typeof__(*p) __v = ACCESS_ONCE(*p);        \
     smp_mb();                                    \
     __v;                                         \
     })
+#endif
+
 #define smp_load_consume(p)                      \
     ({                                           \
     __typeof__(*p) __v = ACCESS_ONCE(*p);        \
@@ -66,12 +89,13 @@
 
 // On arm we produce a zero that is data dependent on an
 // argument by xoring it with itself.
-#define dependent_zero(v)                                   \
-    ({                                                      \
-    __typeof__(v) __i = v;                                  \
-    __asm__ ("eor %[val], %[val];" : [val] "+r" (__i) ::);  \
-    __i;                                                    \
+#define dependent_zero(v)                                               \
+    ({                                                                  \
+    __typeof__(v) __i = v;                                              \
+    __asm__ __volatile__("eor %[val], %[val];" : [val] "+r" (__i) ::);  \
+    __i;                                                                \
     })
+
 #else
 #error CPU not supported
 #endif
