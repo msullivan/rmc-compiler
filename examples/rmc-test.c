a0b104941bb518caa627c79a0a6315c226961496
Michael Sullivan <sully@msully.net>
2014-09-25 18:03:52 -0400

    Add a bunch of rmc tests that we brutally miscompile because llvm optimizes everything away.

---
+++ examples/rmc-test.c
@@ -0,0 +1,44 @@
+#include "rmc.h"
+
+// HMMMMM. All of these get optimized away and shouldn't.
+// Also, if r doesn't get used usefully, that load gets optimized away.
+// I can't decide whether that is totally fucked or not.
+
+int global_p, global_q;
+
+int bogus_ctrl_dep1() {
+    XEDGE(read, write);
+
+    L(read, int r = global_p);
+    if (r == r) {
+        L(write, global_q = 1);
+    }
+
+    return r;
+}
+
+// Do basically the same thing in each branch
+int bogus_ctrl_dep2() {
+    XEDGE(read, write);
+
+    L(read, int r = global_p);
+    if (r) {
+        L(write, global_q = 1);
+    } else {
+        L(write, global_q = 1);
+    }
+
+    return r;
+}
+
+// Have a bogus ctrl dep
+int bogus_ctrl_dep3() {
+    XEDGE(read, write);
+
+    L(read, int r = global_p);
+    if (r) {};
+
+    L(write, global_q = 1);
+
+    return r;
+}
