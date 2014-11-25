4daf51b49667359b113d43fa859967cfe63858de
Michael Sullivan <sully@msully.net>
2014-11-24 20:01:50 -0500

    Implement a test that demonstrates the exponential blowup of our system

---
+++ examples/badness.c
@@ -0,0 +1,34 @@
+#include "rmc.h"
+
+extern int coin(void);
+
+// A test that should be really bad because exponents.
+// Only takes like a minute or so!
+void welp(int *p, int *q) {
+    VEDGE(a, b);
+    L(a, *p = 1);
+
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    // 4
+
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    // 8
+
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    if (coin()){}
+    // 16
+
+    L(b, *q = 1);
+}
