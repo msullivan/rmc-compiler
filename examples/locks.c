f1a5993fad311cc7a065e8f4f3a629224c143920
Michael Sullivan <sully@msully.net>
2015-02-18 17:36:42 -0500

    Write some ticket taking locks

--- examples/locks.c
+++ examples/locks.c
@@ -33,3 +33,34 @@ void mutex_unlock(mutex_t *lock)
 int nus(int *x) {
     return rmc_fetch_and(x, 1337);
 }
+
+/// Ticket taking ones
+typedef struct mutex2_t {
+    int next;
+    int owner;
+} mutex2_t;
+
+void mutex2_lock_bad(mutex2_t *lock)
+{
+    XEDGE(take, check);
+    XEDGE(check, post);
+    int ticket = L(take, rmc_fetch_add(&lock->next, 1));
+    while (ticket != L(check, lock->owner)) {}
+}
+
+void mutex2_lock(mutex2_t *lock)
+{
+    //XEDGE(take, check); // I /think/ maybe we don't need this depending.
+    XEDGE(check, loop_out);
+    XEDGE(loop_out, post);
+
+    int ticket = L(take, rmc_fetch_add(&lock->next, 1));
+    while (ticket != L(check, lock->owner)) {}
+    L(loop_out, 0);
+}
+
+void mutex2_unlock(mutex2_t *lock)
+{
+    VEDGE(pre, unlock);
+    L(unlock, lock->owner++);
+}
