ad63f95e203ba5f5fe7e9fc5abf3c357a79d014f
Michael Sullivan <sully@msully.net>
2015-03-05 13:33:30 -0500

    Add a (broken) test for consume-style things where we compare the pointer.

---
+++ examples/consume-compare-test.c
@@ -0,0 +1,16 @@
+#define REQUIRE_EXPLICIT_ATOMICS 1
+#include "rmc.h"
+
+int foo;
+
+// LLVM will want to propagate the knowledge that p == &foo into the
+// conditional, breaking our chain. We need to make sure that doesn't
+// happen. The optimization is performed by -gvn.
+int test(_Rmc(int *)* pp) {
+    XEDGE(a, b);
+    int *p = L(a, rmc_load(pp));
+    if (p == &foo) {
+        return L(b, *p);
+    }
+    return -1;
+}
