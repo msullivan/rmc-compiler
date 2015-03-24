4868765b618b6edf018065fe75a84e2b3e49ded5
Michael Sullivan <sully@msully.net>
2015-03-24 16:56:24 -0400

    Fix some silliness in regen.sh

--- experiments/regen.sh
+++ experiments/regen.sh
@@ -8,12 +8,12 @@ powerpc-linux-gnu-g++-4.9 $FLAGS fragments.cpp -o fragments-gcc-power.s
 
 arm-linux-gnueabihf-g++-4.9 $FLAGS fragments.cpp -o fragments-gcc-arm.s
 
-LLVM_POWER_FLAGS="--target=powerpc fragments.cpp -I /usr/powerpc-linux-gnu/include/ -I /usr/powerpc-linux-gnu/include/c++/4.9.1/ -I /usr/powerpc-linux-gnu/include/c++/4.9.1/powerpc-linux-gnu"
-LLVM_ARM_FLAGS="--target=armv7a fragments.cpp -mfloat-abi=hard -I /usr/arm-linux-gnueabihf/include/ -I /usr/arm-linux-gnueabihf/include/c++/4.9.1/ -I /usr/arm-linux-gnueabihf/include/c++/4.9.1/arm-linux-gnueabihf"
+LLVM_POWER_FLAGS="--target=powerpc -I /usr/powerpc-linux-gnu/include/ -I /usr/powerpc-linux-gnu/include/c++/4.9.1/ -I /usr/powerpc-linux-gnu/include/c++/4.9.1/powerpc-linux-gnu"
+LLVM_ARM_FLAGS="--target=armv7a -mfloat-abi=hard -I /usr/arm-linux-gnueabihf/include/ -I /usr/arm-linux-gnueabihf/include/c++/4.9.1/ -I /usr/arm-linux-gnueabihf/include/c++/4.9.1/arm-linux-gnueabihf"
 
 ../../build-opt/Release+Asserts/bin/clang $FLAGS fragments.cpp -o fragments-llvm-x86.s
-../../build-opt/Release+Asserts/bin/clang $FLAGS $LLVM_POWER_FLAGS -o fragments-llvm-power.s
-../../build-opt/Release+Asserts/bin/clang $FLAGS $LLVM_ARM_FLAGS -o fragments-llvm-arm.s
+../../build-opt/Release+Asserts/bin/clang $FLAGS fragments.cpp $LLVM_POWER_FLAGS -o fragments-llvm-power.s
+../../build-opt/Release+Asserts/bin/clang $FLAGS fragments.cpp $LLVM_ARM_FLAGS -o fragments-llvm-arm.s
 
 ../../build-opt/Release+Asserts/bin/clang $FLAGS fragments.cpp -o fragments-llvm-x86.ll -emit-llvm
 ../../build-opt/Release+Asserts/bin/clang $FLAGS $LLVM_POWER_FLAGS -o fragments-llvm-power.ll -emit-llvm
