c68fe59905a4fd428f80b880841a8d2669d2c361
Michael Sullivan <sully@msully.net>
2015-10-22 18:21:17 -0400

    Make the rust plugin report the llvm error when it fails

--- rust/plugin/src/lib.rs
+++ rust/plugin/src/lib.rs
@@ -6,7 +6,7 @@
 extern crate rustc;
 use rustc::plugin::Registry;
 extern crate rustc_llvm;
-use std::ffi::CString;
+use std::ffi::{CString,CStr};
 
 use std::process::Command;
 
@@ -18,8 +18,12 @@ pub fn plugin_registrar(reg: &mut Registry) {
         });
 
     let s = CString::new(output.stdout).unwrap();
-    if unsafe {rustc_llvm::LLVMRustLoadDynamicLibrary(s.as_ptr()) } == 0 {
-        panic!("couldn't load rmc library")
+    unsafe {
+        if rustc_llvm::LLVMRustLoadDynamicLibrary(s.as_ptr()) == 0 {
+            let err = rustc_llvm::LLVMRustGetLastError();
+            let s = CStr::from_ptr(err).to_string_lossy().into_owned();
+            panic!("couldn't load rmc library: {}", s)
+        }
     }
     reg.register_llvm_pass("realize-rmc");
     reg.register_llvm_pass("simplifycfg");
