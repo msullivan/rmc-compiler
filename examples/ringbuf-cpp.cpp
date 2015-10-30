8f446a9f156d70426f0dbedd0fe6653b2f08b31d
Michael Sullivan <sully@msully.net>
2015-10-30 15:27:19 -0400

    Add a C++ version of the ringbuf

---
+++ examples/ringbuf-cpp.cpp
@@ -0,0 +1,43 @@
+#include <rmc++.h>
+
+#define BUF_SIZE (1024*1)
+
+struct ring_buf {
+    unsigned char buf[BUF_SIZE];
+    rmc::atomic<unsigned> front, back;
+};
+
+
+int buf_enqueue(ring_buf *buf, unsigned char c)
+{
+    XEDGE(e_check, insert);
+    VEDGE(insert, e_update);
+
+    unsigned back = buf->back;
+    unsigned front = L(e_check, buf->front);
+
+    int enqueued = 0;
+    if (back - BUF_SIZE != front) {
+        L(insert, buf->buf[back % BUF_SIZE] = c);
+        L(e_update, buf->back = back + 1);
+        enqueued = 1;
+    }
+    return enqueued;
+}
+
+int buf_dequeue(ring_buf *buf)
+{
+    XEDGE(d_check, read);
+    XEDGE(read, d_update);
+
+    unsigned front = buf->front;
+    unsigned back = L(d_check, buf->back);
+
+    int c = -1;
+    if (front != back) {
+        L(read, c = buf->buf[front % BUF_SIZE]);
+        L(d_update, buf->front = front + 1);
+    }
+
+    return c;
+}
