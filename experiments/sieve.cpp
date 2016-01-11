67d795e86803f3d0a4cebeecaf2f52cd28427c0d
Michael Sullivan <sully@msully.net>
2016-01-11 16:21:55 -0500

    Implement of Sieve of Eratothenes test

---
+++ experiments/sieve.cpp
@@ -0,0 +1,109 @@
+// Parallel Sieve of Eratsothenes test, inspired by the example in
+// "Threads Cannot be Implemented as a Library".
+
+// The main thing that I wanted to test here was how much gain there
+// is from adding SC atomics to RMC. Answer: a lot,
+// probably. MFENCEing on ever get() is like a 3x slowdown, and that
+// is what SC would require without adding it to RMC.
+
+#include <atomic>
+#include <thread>
+#include <iostream>
+#include <vector>
+
+#ifndef MODE
+#define MODE UnsafeByte
+#endif
+
+const int kSqrt = 40000;
+const int kMax = kSqrt*kSqrt;
+
+// We allocate one big array and cast as necessary. Allocating an array
+// for each possible scheme lead to linker errors.
+char bigArray[kMax];
+std::atomic<bool>* atomicArray =
+    reinterpret_cast<std::atomic<bool> *>(bigArray);
+
+////// SC C++11 atomics
+// Looks like this is about a 50% regression against relaxed
+struct SCByte {
+    static std::atomic<bool>* bits;
+    static void set(int idx) { bits[idx] = true; }
+    static bool get(int idx) { return bits[idx]; }
+};
+std::atomic<bool>* SCByte::bits = atomicArray;
+
+////// SC atomics if we had to MFENCE after loads also
+// Ouch! This is like a 3x regression against the SC one!
+struct SCBadByte {
+    static std::atomic<bool>* bits;
+    static void set(int idx) { bits[idx] = true; }
+    static bool get(int idx) {
+        // Both fence locations cause huge grief.
+        //std::atomic_thread_fence(std::memory_order_seq_cst);
+        bool r = bits[idx];
+        std::atomic_thread_fence(std::memory_order_seq_cst);
+        return r;
+    }
+};
+std::atomic<bool>* SCBadByte::bits = atomicArray;
+
+////// Relaxed C++11 atomics
+// Should perform about as well as UnsafeByte
+struct RelaxedByte {
+    static std::atomic<bool>* bits;
+    static void set(int idx) {
+        bits[idx].store(true, std::memory_order_relaxed);
+    }
+    static bool get(int idx) {
+        return bits[idx].load(std::memory_order_relaxed);
+    }
+};
+std::atomic<bool>* RelaxedByte::bits = atomicArray;
+
+////// Unsafe accesses
+struct UnsafeByte {
+    static bool* bits;
+    static void set(int idx) { bits[idx] = true; }
+    static bool get(int idx) { return bits[idx]; }
+};
+bool* UnsafeByte::bits = reinterpret_cast<bool*>(bigArray);
+
+// The actual sieve, parameterized against an Array that tracks bits
+template<class Array>
+void sieve(int start) {
+    for (int my_prime = start; my_prime < kSqrt; my_prime++) {
+        if (!Array::get(my_prime)) {
+            for (int multiple = my_prime; multiple < kMax;
+                 multiple += my_prime) {
+                if (!Array::get(multiple)) Array::set(multiple);
+            }
+        }
+    }
+}
+
+template<class Array>
+void test(int nthreads) {
+    std::vector<std::thread> threads;
+
+    // XXX: we should probably synchronize thread starting work and
+    // just time the actual work.
+    for (int i = 0; i < nthreads; i++) {
+        threads.push_back(std::thread(sieve<Array>, 2));
+    }
+
+    for (auto & thread : threads) {
+        thread.join();
+    }
+
+    std::cout << "86028157 should be prime: " << Array::get(86028157) << "\n";
+    std::cout << "99999997 should be composite: " <<
+        Array::get(99999997) << "\n";
+}
+
+int main(int argc, char** argv) {
+    int nthreads = argc > 1 ? atoi(argv[1]) : 1;
+    test<MODE>(nthreads);
+
+    return 0;
+}
