8607919a984c30f5fdb42e8253fcb755b19aff54
Michael Sullivan <sully@msully.net>
2016-01-12 16:07:18 -0500

    Update the year on the Copyright headers.
    find . -type f | xargs sed -i 's/2014-2015 Michael J. Sullivan/2014-2016 Michael J. Sullivan/

--- PathCache.cpp
+++ PathCache.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2014-2015 Michael J. Sullivan
+// Copyright (c) 2014-2016 Michael J. Sullivan
 // Use of this source code is governed by an MIT-style license that can be
 // found in the LICENSE file.
 
