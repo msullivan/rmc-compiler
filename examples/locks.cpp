3d7b488c39c8b7fdf32d4d21b9f3f977b528e048
Michael Sullivan <sully@msully.net>
2016-01-13 17:55:04 -0500

    Rewrite the locks example to use the C++ API

--- examples/locks.cpp
+++ examples/locks.cpp
@@ -1,51 +1,52 @@
-typedef bool _Bool;
-#include <rmc.h>
+#include <rmc++.h>
 
 typedef struct mutex_t {
-    rmc_int locked;
+    rmc::atomic<int> locked;
 } mutex_t;
 
 void mutex_lock_bad(mutex_t *lock)
 {
-    XEDGE(lock, post);
+    XEDGE(trylock, post);
     int expected;
     do {
         expected = 0;
-    } while (!L(lock, rmc_compare_exchange_weak(&lock->locked, &expected, 1)));
+    } while (!L(trylock, lock->locked.compare_exchange_weak(expected, 1)));
 }
 
 void mutex_lock(mutex_t *lock)
 {
-    XEDGE(lock, loop_out);
+    XEDGE(trylock, loop_out);
     int expected;
     do {
         expected = 0;
-    } while (L(lock, rmc_compare_exchange_weak(&lock->locked, &expected, 1)) == 0);
+    } while (!L(trylock, lock->locked.compare_exchange_weak(expected, 1)));
     LPOST(loop_out);
 }
 
 void mutex_unlock(mutex_t *lock)
 {
     VEDGE(pre, unlock);
-    L(unlock, rmc_store(&lock->locked, 0));
+    L(unlock, lock->locked = 0);
 }
 
-int nus(rmc_int *x) {
-    return rmc_fetch_and(x, 1337);
+int nus(rmc::atomic<int> *x) {
+    return x->fetch_and(1337);
 }
+
 
 /// Ticket taking ones
 typedef struct mutex2_t {
-    rmc_int next;
-    rmc_int owner;
+    rmc::atomic<int> next;
+    rmc::atomic<int> owner;
 } mutex2_t;
 
 void mutex2_lock_bad(mutex2_t *lock)
 {
     XEDGE(take, check);
     XEDGE(check, post);
-    int ticket = L(take, rmc_fetch_add(&lock->next, 1));
-    while (ticket != L(check, rmc_load(&lock->owner))) {}
+    int ticket = L(take, lock->next.fetch_add(1));
+    while (ticket != L(check, lock->owner))
+        continue;
 }
 
 void mutex2_lock(mutex2_t *lock)
@@ -53,13 +54,14 @@ void mutex2_lock(mutex2_t *lock)
     //XEDGE(take, check); // I /think/ maybe we don't need this depending.
     XEDGE(check, loop_out);
 
-    int ticket = L(take, rmc_fetch_add(&lock->next, 1));
-    while (ticket != L(check, rmc_load(&lock->owner))) {}
+    int ticket = L(take, lock->next.fetch_add(1));
+    while (ticket != L(check, lock->owner))
+        continue;
     LPOST(loop_out);
 }
 
 void mutex2_unlock(mutex2_t *lock)
 {
     VEDGE(pre, unlock);
-    L(unlock, rmc_store(&lock->owner, rmc_load(&lock->owner)+1));
+    L(unlock, lock->owner = lock->owner + 1);
 }
