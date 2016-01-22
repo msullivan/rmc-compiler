5773c3e927a7eb36bf0ab3d3944ceed1807c64af
Michael Sullivan <sully@msully.net>
2016-01-22 17:02:07 -0500

    Implement Michael-Scott queues in C++11 SC fragment, write a test

---
+++ examples/case_studies/ms_queue-test.cpp
@@ -0,0 +1,73 @@
+#include <thread>
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <experimental/optional>
+
+#include "ms_queue_sc.hpp"
+
+int kCount = 1000000;
+
+rmclib::MSQueue<int> queue;
+std::atomic<bool> producersDone{false};
+
+void producer() {
+    for (int i = 0; i < kCount; i++) {
+        queue.enqueue(i);
+    }
+}
+
+std::atomic<long> totalSum{0};
+void consumer() {
+    long sum = 0;
+    for (;;) {
+        auto res = queue.dequeue();
+        if (!res) {
+            if (producersDone) break;
+        } else {
+            sum += *res;
+        }
+    }
+    // Stupidly, this winds up being "more atomic" than using cout
+    printf("Done: %ld\n", sum);
+    totalSum += sum;
+}
+
+void test(int nproducers, int nconsumers) {
+    std::vector<std::thread> producers;
+    std::vector<std::thread> consumers;
+
+    // XXX: we should probably synchronize thread starting work and
+    // just time the actual work.
+    for (int i = 0; i < nproducers; i++) {
+        producers.push_back(std::thread(producer));
+    }
+    for (int i = 0; i < nconsumers; i++) {
+        consumers.push_back(std::thread(consumer));
+    }
+
+    for (auto & thread : producers) {
+        thread.join();
+    }
+    producersDone = true;
+    for (auto & thread : consumers) {
+        thread.join();
+    }
+    printf("Final sum: %ld\n", totalSum.load());
+
+}
+
+int main(int argc, char** argv) {
+    int producers = 1, consumers = 1;
+    if (argc == 2) {
+        int total = atoi(argv[1]);
+        producers = total/2;
+        consumers = total - producers;
+    } if (argc == 3) {
+        producers = atoi(argv[1]);
+        consumers = atoi(argv[2]);
+    }
+    test(producers, consumers);
+
+    return 0;
+}
---
+++ examples/case_studies/ms_queue_sc.hpp
@@ -0,0 +1,142 @@
+#ifndef RMC_MS_QUEUE_SC
+#define RMC_MS_QUEUE_SC
+
+#include <atomic>
+#include <utility>
+#include <experimental/optional>
+
+namespace rmclib {
+#if 0
+} // f. this
+#endif
+
+// c.c trolling?
+template<class T> using lf_ptr = T*;
+const int kCacheLinePadding = 128; // I have NFI
+
+// I'm doing this all C++ified, but maybe I shouldn't be.
+
+// XXX: T should have a trivial destructor
+// XXX: in light of that, maybe this move nonsense is nonsense.
+// although if we epoch or gc or whatever it is fine...
+// and this code isn't set up for not doing that...
+template<typename T>
+class MSQueue {
+private:
+    struct MSQueueNode {
+        std::atomic<lf_ptr<MSQueueNode>> next_{nullptr};
+        T data_;
+
+        MSQueueNode() {} // needed for allocating dummy
+        MSQueueNode(T &&t) : data_(std::move(t)) {} // is this right
+        MSQueueNode(const T &t) : data_(t) {}
+    };
+
+
+    // XXX: alignment
+    std::atomic<lf_ptr<MSQueueNode>> head_{nullptr};
+    std::atomic<lf_ptr<MSQueueNode>> tail_{nullptr};
+
+    void enqueue_node(lf_ptr<MSQueueNode> node);
+
+public:
+    MSQueue() {
+        // Need to create a dummy node!
+        head_ = tail_ = new MSQueueNode();
+    }
+
+    std::experimental::optional<T> dequeue();
+
+    // XXX: allocations!
+    void enqueue(T &&t) {
+        enqueue_node(new MSQueueNode(std::move(t)));
+    }
+    void enqueue(const T &t) {
+        enqueue_node(new MSQueueNode(t));
+    }
+};
+
+template<typename T>
+void MSQueue<T>::enqueue_node(lf_ptr<MSQueueNode> node) {
+    // XXX: start epoch
+
+    for (;;) {
+        lf_ptr<MSQueueNode> tail = this->tail_;
+        lf_ptr<MSQueueNode> next = tail->next_;
+        // Check that tail and next are consistent:
+        // If we are using an epoch/gc based approach
+        // (which we had better be, since we don't have gen counters),
+        // this is purely an optimization.
+        if (tail != this->tail_) continue;
+
+        // was tail /actually/ the last node?
+        if (next == nullptr) {
+            // if so, try to write it in. (nb. this overwrites next)
+            // XXX: does weak actually help us here?
+            if (tail->next_.compare_exchange_weak(next, node)) {
+                // we did it! return
+                break;
+            }
+        } else {
+            // nope. try to swing the tail further down the list and try again
+            this->tail_.compare_exchange_strong(tail, next);
+        }
+    }
+
+    // XXX: end epoch?
+}
+
+template<typename T>
+std::experimental::optional<T> MSQueue<T>::dequeue() {
+    lf_ptr<MSQueueNode> head, tail, next;
+
+    for (;;) {
+        head = this->head_;
+        tail = this->tail_;
+        next = head->next_;
+
+        // Consistency check; see note above
+        if (head != this->head_) continue;
+
+        // Check if the queue *might* be empty
+        // XXX: is it necessary to have the empty check under this
+        if (head == tail) {
+            // Ok, so, the queue might be empty, but it also might
+            // be that the tail pointer has just fallen behind.
+            // If the next pointer is null, then it is actually empty
+            if (next == nullptr) {
+                return std::experimental::optional<T>{};
+            } else {
+                // not empty: tail falling behind; since it is super
+                // not ok for the head to advance past the tail,
+                // try advancing the tail
+                // XXX weak v strong?
+                this->tail_.compare_exchange_strong(tail, next);
+            }
+        } else {
+            // OK, now we try to actually read the thing out.
+
+            // If we weren't planning to rely on epochs or something,
+            // note that we would need to read out the data *before* we
+            // do the CAS, or else things are gonna get bad.
+            // (could get reused first)
+            if (this->head_.compare_exchange_weak(head, next)) {
+                break;
+            }
+        }
+    }
+
+    // OK, everything set up.
+    // next contains the value we are reading
+    // head can be freed
+    //epoch_free(head); // XXX or something
+    std::experimental::optional<T> ret(std::move(next->data_));
+    next->data_.~T(); // call destructor
+
+    return ret;
+}
+
+}
+
+
+#endif
