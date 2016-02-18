ea1c2ef60aa6326111d94b428db2937b61669d9b
Michael Sullivan <sully@msully.net>
2016-02-17 19:39:31 -0800

    Make the experiments test bench have a mode better suited to dual-core

--- experiments/test.c
+++ experiments/test.c
@@ -29,27 +29,60 @@ int go;
 bool done[MAX_THREADS];
 int results[MAX_THREADS];
 
+void iteration(int thread, int *count) {
+    test_fn *f = test_fns[thread];
+    while (smp_load_acquire(&go) == *count)
+        ;
+    int r = f();
+    results[thread] = r;
+    smp_store_release(&done[thread], true);
+
+    count++;
+}
+
 void *tester_thread(void *pthread)
 {
     int thread = (int)(long)pthread;
-    test_fn *f = test_fns[thread];
     int count = 0;
     while (1) {
-        while (smp_load_acquire(&go) == count)
-            ;
-        int r = f();
-        results[thread] = r;
-        smp_store_release(&done[thread], true);
-
-        count++;
+        iteration(thread, &count);
     }
     return NULL;
 }
 
+// Argh. On dual-core machines it doesn't do very well to have one
+// thread orchestrating the stuff in addition to the test threads. Be
+// able to use one of the test threads to orchestrate.
+#ifdef CORE_SHORTAGE
+
+#define FIRST_THREAD 1
 void run_test(int threads, int n)
 {
     reset();
-    results[1] = 1;
+    // Let my people go
+    smp_store_release(&go, n+1);
+
+    // Run the test ourself
+    iteration(0, &n);
+
+    // Wait for everyone else
+    for (int i = 0; i < threads; i++) {
+        while (!smp_load_acquire(&done[i]))
+            ;
+
+        smp_store_release(&done[i], false);
+    }
+
+    // Everyone done, process results.
+    process_results(results);
+}
+
+#else
+
+#define FIRST_THREAD 0
+void run_test(int threads, int n)
+{
+    reset();
     // Let my people go
     smp_store_release(&go, n+1);
     // Wait for everyone
@@ -64,6 +97,8 @@ void run_test(int threads, int n)
     process_results(results);
 }
 
+#endif
+
 void run_tests(int threads, int count)
 {
     for (int i = 0; i < count; i++) {
@@ -71,12 +106,11 @@ void run_tests(int threads, int count)
         run_test(threads, i);
     }
 }
-
 int main(int argc, char **argv)
 {
     int iterations = (argc == 2) ? atoi(argv[1]) : ITERATIONS;
 
-    for (int i = 0; i < thread_count; i++) {
+    for (int i = FIRST_THREAD; i < thread_count; i++) {
         pthread_t thread;
         pthread_create(&thread, NULL, tester_thread, (void *)(long)i);
     }
