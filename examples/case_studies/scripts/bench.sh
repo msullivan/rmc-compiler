d5fba84a7c676bb0a2ee3ac3c326a392b1c0af8f
Michael Sullivan <sully@msully.net>
2016-02-25 17:36:49 -0800

    Fiddle with bench.sh

--- examples/case_studies/scripts/bench.sh
+++ examples/case_studies/scripts/bench.sh
@@ -2,13 +2,21 @@
 
 ITERATIONS=$1
 shift
-PROGRAM=$1
+TAG=$1
 shift
-ARGS="$@"
+# seriously shell scripts are the worst and I should stop
+ARGS="$1"
+shift
+PROGRAMS="$@"
 
 for I in `seq $ITERATIONS`
 do
+    for PROGRAM in $PROGRAMS
+    do
+	FILE=data/$PROGRAM-$TAG.csv
 	printf "%d,%s,%s %s,$PROGRAM,$ARGS," $I `hostname` \
-		   `date --rfc-3339=seconds -u`
-	$PROGRAM -b "$ARGS"
+		   `date --rfc-3339=seconds -u` >> $FILE
+	./$PROGRAM -b "$ARGS" >> $FILE
+	#sleep 60 # evade, don't solve, temperature problems
+    done
 done
