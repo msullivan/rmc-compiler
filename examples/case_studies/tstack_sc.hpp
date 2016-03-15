8f5c2116773cf08c40a7b66bae19e866bc99990b
Michael Sullivan <sully@msully.net>
2016-03-15 14:59:42 -0700

    Refactor tstack to put most stuff in a tstack_shared.hpp

--- examples/case_studies/tstack_sc.hpp
+++ examples/case_studies/tstack_sc.hpp
@@ -1,56 +1,8 @@
 #ifndef RMC_TSTACK_SC
 #define RMC_TSTACK_SC
 
-#include <atomic>
-#include <utility>
-#include "util.hpp"
-#include "epoch.hpp"
 #define UnsafeTStack UnderlyingTStack
 #include "unsafe_tstack_sc.hpp"
-
-namespace rmclib {
-
-// I'm doing this all C++ified, but maybe I shouldn't be.
-template<typename T>
-class TStack {
-private:
-    using Stack = UnderlyingTStack<optional<T>>;
-    using TStackNode = typename Stack::TStackNode;
-    Stack stack_;
-
-    void pushNode(TStackNode *node) {
-        auto guard = Epoch::pin();
-        stack_.pushNode(node);
-    }
-
-public:
-    TStack() { }
-
-    optional<T> pop();
-
-    void push(T &&t) {
-        pushNode(new TStackNode(std::move(t)));
-    }
-    void push(const T &t) {
-        pushNode(new TStackNode(t));
-    }
-};
-
-template<typename T>
-optional<T> TStack<T>::pop() {
-    auto guard = Epoch::pin();
-    lf_ptr<TStackNode> node = stack_.popNode();
-
-    if (!node) return optional<T>{};
-
-    optional<T> ret(std::move(node->data_));
-    node->data_ = optional<T>{}; // destroy the object
-    guard.unlinked(node);
-
-    return ret;
-}
-
-}
-
+#include "tstack_shared.hpp"
 
 #endif
---
+++ examples/case_studies/tstack_shared.hpp
@@ -0,0 +1,55 @@
+#ifndef RMC_TSTACK_SHARED
+#define RMC_TSTACK_SHARED
+
+// An UnderlyingTStack must have already been defined.
+
+#include <atomic>
+#include <utility>
+#include "util.hpp"
+#include "epoch.hpp"
+
+namespace rmclib {
+
+// I'm doing this all C++ified, but maybe I shouldn't be.
+template<typename T>
+class TStack {
+private:
+    using Stack = UnderlyingTStack<optional<T>>;
+    using TStackNode = typename Stack::TStackNode;
+    Stack stack_;
+
+    void pushNode(TStackNode *node) {
+        auto guard = Epoch::pin();
+        stack_.pushNode(node);
+    }
+
+public:
+    TStack() { }
+
+    optional<T> pop();
+
+    void push(T &&t) {
+        pushNode(new TStackNode(std::move(t)));
+    }
+    void push(const T &t) {
+        pushNode(new TStackNode(t));
+    }
+};
+
+template<typename T>
+optional<T> TStack<T>::pop() {
+    auto guard = Epoch::pin();
+    lf_ptr<TStackNode> node = stack_.popNode();
+
+    if (!node) return optional<T>{};
+
+    optional<T> ret(std::move(node->data_));
+    node->data_ = optional<T>{}; // destroy the object
+    guard.unlinked(node);
+
+    return ret;
+}
+
+}
+
+#endif
