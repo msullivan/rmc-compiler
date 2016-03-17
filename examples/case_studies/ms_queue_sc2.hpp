978a5223cf6f3bfd026d3cf7df1eafea05f8157b
Michael Sullivan <sully@msully.net>
2016-03-16 19:57:10 -0700

    Add a broken ms_queue that uses freelists

---
+++ examples/case_studies/ms_queue_sc2.hpp
@@ -0,0 +1,150 @@
+#ifndef RMC_MS_QUEUE_SC
+#define RMC_MS_QUEUE_SC
+
+#include <atomic>
+#include <utility>
+#include "util.hpp"
+#include "freelist.hpp"
+
+// A version of Michael-Scott queues that closely corresponds to the
+// original version using generation pointers.
+
+namespace rmclib {
+
+template<typename T>
+class MSQueue {
+private:
+    struct MSQueueNode {
+        std::atomic<lf_ptr<MSQueueNode>> next_{nullptr};
+        optional<T> data_;
+
+        MSQueueNode() {}
+    };
+
+
+    alignas(kCacheLinePadding)
+    std::atomic<lf_ptr<MSQueueNode>> head_{nullptr};
+    alignas(kCacheLinePadding)
+    std::atomic<lf_ptr<MSQueueNode>> tail_{nullptr};
+
+    Freelist<MSQueueNode> freelist_;
+
+    void enqueueNode(lf_ptr<MSQueueNode> node);
+
+public:
+    MSQueue() {
+        // Need to create a dummy node!
+        auto node = freelist_.alloc();
+        node->next_ = nullptr;
+        head_ = tail_ = node;
+    }
+
+    optional<T> dequeue();
+
+    void enqueue(T &&t) {
+        auto node = freelist_.alloc();
+        node->data_ = std::move(t);
+        enqueueNode(node);
+    }
+    void enqueue(const T &t) {
+        auto node = freelist_.alloc();
+        node->data_ = t;
+        enqueueNode(node);
+    }
+};
+
+template<typename T>
+void MSQueue<T>::enqueueNode(lf_ptr<MSQueueNode> node) {
+    node->next_ = nullptr;
+
+    lf_ptr<MSQueueNode> tail, next;
+
+    for (;;) {
+        tail = this->tail_;
+        next = tail->next_;
+        // Check that tail and next are consistent:
+        // If we are using an epoch/gc based approach
+        // (which we had better be, since we don't have gen counters),
+        // this is purely an optimization.
+        if (tail != this->tail_) continue;
+
+        // was tail /actually/ the last node?
+        if (next == nullptr) {
+            // if so, try to write it in. (nb. this overwrites next)
+            // XXX: does weak actually help us here?
+            if (tail->next_.compare_exchange_weak(next, node)) {
+                // we did it! return
+                break;
+            }
+        } else {
+            // nope. try to swing the tail further down the list and try again
+            this->tail_.compare_exchange_strong(tail, next);
+        }
+    }
+
+    // Try to swing the tail_ to point to what we inserted
+    this->tail_.compare_exchange_strong(tail, node);
+}
+
+template<typename T>
+optional<T> MSQueue<T>::dequeue() {
+    lf_ptr<MSQueueNode> head, tail, next;
+
+    optional<T> ret;
+
+    for (;;) {
+        head = this->head_;
+        tail = this->tail_;
+        next = head->next_;
+
+        // Consistency check; see note above
+        if (head != this->head_) continue;
+
+        // Check if the queue *might* be empty
+        // XXX: is it necessary to have the empty check under this
+        if (head == tail) {
+            // Ok, so, the queue might be empty, but it also might
+            // be that the tail pointer has just fallen behind.
+            // If the next pointer is null, then it is actually empty
+            if (next == nullptr) {
+                return optional<T>{};
+            } else {
+                // not empty: tail falling behind; since it is super
+                // not ok for the head to advance past the tail,
+                // try advancing the tail
+                // XXX weak v strong?
+                this->tail_.compare_exchange_strong(tail, next);
+            }
+        } else {
+            // OK, now we try to actually read the thing out.
+
+            // Unfortunately we need to read the data out of the node
+            // *before* we try to dequeue it or else it could get
+            // reused before we read it out.
+            // This unfortunately means we can't move the data out,
+            // which we could in the epoch versions.
+
+            // XXX: oh hey, it's actually *way* worse than that.  It
+            // could get dequeued and reused while we are copying it
+            // out. This is a straight-up data race and I think there
+            // is no way around it but to store the payload in a
+            // std::atomic. That's gonna suck.
+            ret = next->data_;
+            if (this->head_.compare_exchange_weak(head, next)) {
+                break;
+            }
+        }
+    }
+
+    // OK, everything set up.
+    // head can be freed
+    // head->data_ = optional<T>{}; // destroy object in node we free
+    freelist_.unlinked(head);
+
+    return ret;
+}
+
+}
+
+
+#endif
