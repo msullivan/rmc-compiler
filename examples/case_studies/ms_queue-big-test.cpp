00ecf72032feda06d8545b739768db76996acc29
Michael Sullivan <sully@msully.net>
2016-03-21 18:19:18 -0700

    Add a test that exposes a bug in the node freelist based ms queues
    Currently they have racy accesses to the elements being sent (since
    there is no way to read the data field in a way where it doesn't race
    against a write to the node as it is reused). This is obviously
    undefined behavior no matter what the underlying type is but when it
    is something like std::vector with a highly nontrivial copy
    constructor, it can get pretty bad.

---
+++ examples/case_studies/ms_queue-big-test.cpp
@@ -0,0 +1,177 @@
+#include <thread>
+#include <utility>
+#include <iostream>
+#include <vector>
+#include <cassert>
+#include <atomic>
+#include <thread>
+#include <getopt.h>
+
+#include "util.hpp"
+#include "ms_queue.hpp"
+
+using namespace rmclib;
+
+typedef unsigned long ulong;
+
+const ulong kCount = 10000000;
+
+// This version of the test uses elements with nontrivial copy
+// constructors so that a queue implementation that races on the
+// elements will have a really bad day.
+//
+// This currently fails for the node freelist based queues.
+class QElm {
+    std::vector<ulong> vec_;
+public:
+    QElm(ulong l) { vec_.push_back(l); }
+    ulong val() { return vec_[0]; }
+};
+
+struct Test {
+    MSQueue<QElm> queue;
+
+    std::atomic<bool> producersDone{false};
+    std::atomic<ulong> totalSum{0};
+    std::atomic<ulong> totalCount{0};
+
+    const long count;
+    const int producers;
+    const int consumers;
+    Test(int c, int pr, int co) : count(c), producers(pr), consumers(co) {}
+};
+
+void work() {
+    const int kWork = 50;
+    volatile int nus = 0;
+    for (int i = 0; i < kWork; i++) {
+        nus++;
+    }
+}
+
+void producer(Test *t) {
+    for (int i = 1; i < t->count; i++) {
+        work();
+        t->queue.enqueue(i);
+    }
+}
+
+void consumer(Test *t) {
+    ulong max = 0;
+    ulong sum = 0;
+    ulong count = 0; // lurr
+    bool producersDone = false;
+    for (;;) {
+        auto res = t->queue.dequeue();
+        work();
+        if (!res) {
+            // Bail once we fail to do a dequeue /after/ having
+            // observered that the producers are done.  Otherwise we
+            // could fail, the producer enqueues a bunch more,
+            // finishes, and then we exit without dequeueing it.
+            if (producersDone) break;
+            producersDone = t->producersDone;
+        } else {
+            auto elm = *res;
+            ulong val = elm.val();
+            assert_op(val, <, t->count);
+            if (t->producers == 1) {
+                assert_op(val, >, max);
+                if (t->consumers == 1) {
+                    assert_eq(val, max+1);
+                }
+            }
+            max = val;
+            sum += val;
+            count++;
+        }
+    }
+    // Stupidly, this winds up being "more atomic" than using cout
+    //printf("Done: %lu\n", sum);
+    t->totalSum += sum;
+    t->totalCount += count;
+}
+
+static bool verboseOutput = true;
+
+void test(Test &t) {
+    std::vector<std::thread> producers;
+    std::vector<std::thread> consumers;
+
+    // XXX: we should probably synchronize thread starting work and
+    // just time the actual work.
+    rmclib::BenchTimer timer;
+
+    for (int i = 0; i < t.producers; i++) {
+        producers.push_back(std::thread(producer, &t));
+    }
+    for (int i = 0; i < t.consumers; i++) {
+        consumers.push_back(std::thread(consumer, &t));
+    }
+
+    joinAll(producers);
+    t.producersDone = true;
+    joinAll(consumers);
+
+    timer.report(t.count * (t.producers+t.consumers), verboseOutput);
+
+    // This is real dumb, but overflow means we can't use the closed form...
+    ulong expected = 0;
+    for (int i = 0; i < t.count; i++) {
+        expected += i;
+    }
+    expected *= t.producers;
+
+    //printf("Final sum: %ld\n", t.totalSum.load());
+    assert_eq(t.totalCount.load(), t.producers*(t.count-1));
+    assert_eq(t.totalSum.load(), expected);
+}
+
+int main(int argc, char** argv) {
+    int producers = 1, consumers = 1;
+    long count = kCount;
+    int reps = 1;
+    int dups = 1;
+
+    // getopt kind of ugly. I kind of want to use llvm's arg parsing,
+    // which I <3, but it is such a big hammer
+    int opt;
+    while ((opt = getopt(argc, argv, "p:c:n:r:d:b")) != -1) {
+        switch (opt) {
+        case 'p':
+            producers = atoi(optarg);
+            break;
+        case 'c':
+            consumers = atoi(optarg);
+            break;
+        case 'n':
+            count = strtol(optarg, NULL, 0);
+            break;
+        case 'r':
+            reps = atoi(optarg);
+            break;
+        case 'd':
+            dups = atoi(optarg);
+            break;
+        case 'b':
+            verboseOutput = false;
+            break;
+        default:
+            fprintf(stderr, "Argument parsing error\n");
+            return 1;
+        }
+    }
+
+    for (int i = 0; i < reps; i++) {
+        std::vector<std::thread> tests;
+        for (int j = 0; j < dups; j++) {
+            tests.push_back(std::thread([=] {
+                Test t(count, producers, consumers);
+                test(t);
+            }));
+        }
+        joinAll(tests);
+    }
+
+    return 0;
+}
