44321364c92b81514d56312295d123ef9f149af6
Michael Sullivan <sully@msully.net>
2016-04-01 17:52:12 -0400

    Copy the .dir-locals from examples to case_studies

---
+++ case_studies/.dir-locals.el
@@ -0,0 +1,12 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c++-mode
+; setting this overrides my c-offsets-alist to disable namespace indenting
+;  (c-file-style . "k&r")
+  (c-basic-offset . 4)
+  (indent-tabs-mode))
+ (c-mode
+  (c-file-style . "K&R")
+  (c-basic-offset . 4)
+  (indent-tabs-mode)))
