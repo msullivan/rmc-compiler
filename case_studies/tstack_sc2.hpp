e9df650e819dc59a202654985723ec1a7451cc83
Michael Sullivan <sully@msully.net>
2016-08-19 14:49:54 -0400

    Fix tstack_sc2 to actually use the sc stack

--- case_studies/tstack_sc2.hpp
+++ case_studies/tstack_sc2.hpp
@@ -6,7 +6,7 @@
 #define RMC_TSTACK_SC2
 
 #define UnsafeTStackGen UnderlyingTStackGen
-#include "unsafe_tstack_gen_rmc.hpp"
+#include "unsafe_tstack_gen_sc.hpp"
 #include "tstack_shared2.hpp"
 
 #endif
