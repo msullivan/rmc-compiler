76dbd5da70658f433c56acaf046b3291035bf605
Michael Sullivan <sully@msully.net>
2016-10-28 21:16:40 +0000

    Add some more tests having to do with consume ordering

--- examples/consume-compare-test.c
+++ examples/consume-compare-test.c
@@ -1,15 +1,47 @@
+#include <stdlib.h>
 #include <rmc.h>
 
 int foo;
 
+typedef _Rmc(int*) rmc_pint;
+
+
 // LLVM will want to propagate the knowledge that p == &foo into the
 // conditional, breaking our chain. We need to make sure that doesn't
 // happen. The optimization is performed by -gvn.
-int test(_Rmc(int *)* pp) {
-    XEDGE(a, b);
+int test(rmc_pint* pp) {
+    XEDGE_HERE(a, b);
     int *p = L(a, rmc_load(pp));
     if (p == &foo) {
         return L(b, *p);
     }
     return -1;
+}
+
+// UGHHHHHH clang will back propagate equality information
+int test2(rmc_pint* pp) {
+    XEDGE_HERE(a, b);
+    int *p = L(a, rmc_load(pp));
+    int a = L(b, *p);
+    if (p != &foo) abort();
+    return a;
+}
+
+
+
+// UGHHHHHH clang will back propagate equality information
+// through fucking function inlining
+int *test3_part(rmc_pint* pp, int *outp) {
+    XEDGE_HERE(a, b);
+    int *p = L(a, rmc_load(pp));
+    int a = L(b, *p);
+    *outp = a;
+    return p;
+}
+
+int test3(rmc_pint* pp) {
+    int a;
+    int *p = test3_part(pp, &a);
+    if (p != &foo) abort();
+    return a;
 }
---
+++ examples/consume-null-test.cpp
@@ -0,0 +1,34 @@
+#include <rmc++.h>
+#include <stdio.h>
+
+struct foo {
+    int b;
+    int a;
+};
+
+// LLVM will want to propagate the knowledge that p == &foo into the
+// conditional, breaking our chain. We need to make sure that doesn't
+// happen. The optimization is performed by -gvn.
+foo *lookup(rmc::atomic<foo *> &pp) {
+    XEDGE_HERE(a, b);
+    foo *p = L(a, pp);
+
+    if (L(b, p->a) == 10) {
+        return LGIVE(b, p);
+    }
+
+//    printf("ugh: %p\n", p);
+//    printf("ugh\n");
+    return nullptr;
+}
+
+
+
+void user(rmc::atomic<foo *> &pp) {
+    foo *p = lookup(pp);
+    if (p) {
+        printf("thing: %d\n", p->b);
+    } else {
+        printf("well fuck this\n");
+    }
+}
