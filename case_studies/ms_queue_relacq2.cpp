47a30c9d0a4cf1ce144581e1e1b9da11d338367b
Michael Sullivan <sully@msully.net>
2017-04-17 18:19:06 +0000

    Add a relacq2 version of ms_queue

---
+++ case_studies/ms_queue_relacq2.cpp
@@ -0,0 +1,9 @@
+// Copyright (c) 2014-2017 Michael J. Sullivan
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
+#include "ms_queue.hpp"
+
+namespace rmclib {
+Freelist<MSQueueNode> MSQueueNode::freelist;
+}
---
+++ case_studies/ms_queue_relacq2.hpp
@@ -0,0 +1,6 @@
+// Copyright (c) 2014-2017 Michael J. Sullivan
+// Use of this source code is governed by an MIT-style license that can be
+// found in the LICENSE file.
+
+#include "rmc2relacq.h"
+#include "ms_queue_rmc2.hpp"
