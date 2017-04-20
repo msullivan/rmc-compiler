92823541ba2c13554c8c1447fa8f149e35e2d7f3
Michael Sullivan <sully@msully.net>
2017-04-20 17:54:33 +0000

    Update nooby build-benchmark

--- case_studies/scripts/nooby-build-benchmark.sh
+++ case_studies/scripts/nooby-build-benchmark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-make -C .. && make build/ms_queue-ermc-rmc-test && rm build/epoch_rmc.o build/ms_queue-ermc-rmc-test.o && time make build/ms_queue-ermc-rmc-test
+make -C .. && make build/ms_queue-ermc-rmc-test && rm build/epoch_rmc.o build/ms_queue-ermc-rmc-test.o && time make build/ms_queue-ermc-rmc-test && make build/ms_queue-ec11-c11-test && rm build/epoch_c11.o build/ms_queue-ec11-c11-test.o && time make build/ms_queue-ec11-c11-test
