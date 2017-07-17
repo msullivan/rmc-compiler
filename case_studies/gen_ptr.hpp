e0b695e22ca62987270435101e6ab522ba787cc1
Michael Sullivan <sully@msully.net>
2017-07-17 18:43:23 -0400

    Ditch gen_atomic, which was a mistake

--- case_studies/gen_ptr.hpp
+++ case_studies/gen_ptr.hpp
@@ -12,7 +12,6 @@
 
 namespace rmclib {
 
-
 template<typename T>
 class alignas(2*sizeof(uintptr_t)) gen_ptr {
 private:
@@ -36,89 +35,6 @@ public:
 
     gen_ptr<T> inc(T n) { return gen_ptr<T>(n, gen()+1); }
     gen_ptr<T> update(T n) { return this->inc(n); }
-};
-
-}
-
-// This is bullshit, right?
-namespace std {
-
-// A wrapper around std::atomic that stores a pointer with a
-// generation and provides compare_exchange_{weak,strong}_gen()
-// methods that automatically increment the generation counter in
-// the expected value.
-template<typename T>
-class gen_atomic : public atomic<rmclib::gen_ptr<T>> {
-public:
-    using real_type = rmclib::gen_ptr<T>;
-
-    real_type operator=(real_type desired) noexcept {
-        this->store(desired); return desired;
-    }
-
-    bool compare_exchange_weak_gen(
-        real_type& expected, T desired,
-        std::memory_order order = std::memory_order_seq_cst) noexcept {
-
-        return this->compare_exchange_weak(
-            expected, expected.update(desired), order);
-    }
-    bool compare_exchange_strong_gen(
-        real_type& expected, T desired,
-        std::memory_order order = std::memory_order_seq_cst) noexcept {
-
-        return this->compare_exchange_strong(
-            expected, expected.update(desired), order);
-    }
-    bool compare_exchange_weak_gen(
-        real_type& expected, T desired,
-        std::memory_order success, std::memory_order failure) noexcept {
-
-        return this->compare_exchange_weak(
-            expected, expected.update(desired), success, failure);
-    }
-    bool compare_exchange_strong_gen(
-        real_type& expected, T desired,
-        std::memory_order success, std::memory_order failure) noexcept {
-
-        return this->compare_exchange_strong(
-            expected, expected.update(desired), success, failure);
-    }
-
-
-};
-
-}
-
-namespace rmc {
-
-// Same as above, but for rmc
-template<typename T>
-class gen_atomic : public atomic<rmclib::gen_ptr<T>> {
-public:
-    using real_type = rmclib::gen_ptr<T>;
-
-    real_type operator=(real_type desired) noexcept {
-        this->store(desired); return desired;
-    }
-
-    bool compare_exchange_weak_gen(real_type& expected, T desired) noexcept {
-        return this->compare_exchange_weak(
-            expected, expected.update(desired));
-    }
-    bool compare_exchange_strong_gen(real_type& expected, T desired) noexcept {
-        return this->compare_exchange_strong(
-            expected, expected.update(desired));
-    }
-    bool compare_exchange_gen(real_type& expected, T desired) noexcept {
-        return this->compare_exchange(
-            expected, expected.update(desired));
-    }
-
-};
-// Wee.
-template<class T> struct remove_rmc<gen_atomic<T>> {
-    typedef typename gen_atomic<T>::real_type type;
 };
 
 }
