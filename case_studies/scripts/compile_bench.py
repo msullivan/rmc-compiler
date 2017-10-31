d38a2ec8191af6e29bd5c6a00e64f801e9b1302a
Michael Sullivan <sully@msully.net>
2017-10-31 21:52:01 +0000

    make the compile benchmark not count makefile time

--- case_studies/scripts/compile_bench.py
+++ case_studies/scripts/compile_bench.py
@@ -13,21 +13,32 @@ TESTS = [
     ('rculist', 'rculist_user-ec11-%(type)s-test.o'),
     ('ringbuf', 'ringbuf-%(type)s-test.o'),
     ('qspinlock', 'seqlock-lock-%(type)s-test.o'),
+    ('seqlock', 'seqlock-%(type)s-test.o'),
 ]
 
 
 def main(argv):
+    out = open('data/compile_speed.csv', 'a')
     for (name, obj) in TESTS:
         times = []
         for v in VERSIONS:
             real_obj = 'build/' + obj % {'type': v}
             subprocess.call(["rm", "-f", real_obj])
+            # First, find the command to run. My makefile takes >1s on
+            # my armv7 machine to figure out what to run, so we
+            # shouldn't time that.
+            cmd = subprocess.run(['make', 'VERBOSE=1', '-n', real_obj],
+                                 check=True,
+                                 universal_newlines=True,
+                                 stdout=subprocess.PIPE).stdout.strip()
+            # Now actually time the thing we care about.
             start = time.time()
-            subprocess.call(['make', real_obj])
+            subprocess.call(cmd, shell=True)
             end = time.time()
             times += [end-start]
         row = [name] + [str(t) for t in times]
-        print(",".join(row))
+        print(",".join(row), file=out)
+    out.close()
 
 
 if __name__ == '__main__':
